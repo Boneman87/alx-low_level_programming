f466936f215710cc7e3db04b01974b8b4018854d
Saanu <akinniyijoseph@gmail.com>
2022-09-08 22:22:27 -0700

    Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main is entry point
+ * Success is achieved if program returns 0 to main function
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art, \n");
+	return (0);
+}
