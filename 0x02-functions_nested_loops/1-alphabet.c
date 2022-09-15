a75a10b19e2bfe8d1af2668f7e12a45329beca26
Saanu <akinniyijoseph@gmail.com>
2022-09-14 18:47:49 -0700

    Functions nested loop

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,30 +1,19 @@
 #include "main.h"
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * print_alphabet - Printing small letters
  */
-int main(void)
+void print_alphabet(void)
 {
-	int i = 0;
+	int i = 0, j;
 
 	while (i < 10)
 	{
 		_putchar(10);
 		i++;
-		print_alphabet();
-	}
-	return (0);
-}
-/**
- * print_alphabet - Printing small letters
- */
-void print_alphabet(void)
-{
-	int j;
 
-	for (j = 97; j < 123; j++)
-	{
-		_putchar(j);
+		for (j = 97; j < 123; j++)
+		{
+			_putchar(j);
+		}
 	}
 }
