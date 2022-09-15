9b639e15a75b5f279700ee0ce27584704ed854c3
Saanu <akinniyijoseph@gmail.com>
2022-09-14 23:10:17 -0700

    functions with nested loop

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,11 +6,12 @@
  */
 int main(void)
 {
-	char str1[] = {' ','_', 'p', 'u', 't', 'c', 'h', 'a', 'r', '\0'};
+	char str1[] = {' ', '_', 'p', 'u', 't', 'c', 'h', 'a', 'r', '\0'};
 	int i;
 
-	for (i = 0; str1[i]; i++)
+	for (i = 0; i < 10; i++)
 	{
+		str1[i] = i + 1;
 		_putchar(i);
 	}
 	return (0);
