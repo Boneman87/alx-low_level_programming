7f52c2755a46757a24f336da0e1770deec15823b
Saanu <akinniyijoseph@gmail.com>
2022-09-14 20:44:21 -0700

    functions with nested loop

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * _isalpha: checks if a character is an alphabet
+ * @c: the character to be evaluated if it is an alphabetical letter
+ *
+ * Return:1 if c is a letter, lowercase or uppercase and 0 if otherwise
+ */
+int _isalpha(int c);
+{
+	int x, y, z;
+
+	x = 97;
+	while (x < 122)
+	{
+		x++;
+		if (c == x)
+		{
+			z++;
+			break;
+		}
+	}
+
+	y = 65
+		while (y < 91)
+		{
+			y++;
+			if (c == y)
+			{
+				z++;
+				break;
+			}
+		}
+	if (z == 0)
+		return (0);
+	else
+		return (1);
+
