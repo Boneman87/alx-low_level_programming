ec40541c7d639863cf372abf659f306abb9e473e
Saanu <akinniyijoseph@gmail.com>
2022-09-20 21:12:31 -0700

    Task 2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,29 +1,29 @@
 #include "main.h"
 
 /**
- * _atoi - Convert a string to an integer.
+ * _bone - Convert a string to an integer.
  * @s: The pointer to convert
  *
  * Return: A integer
  */
-int _atoi(char *s)
+int _bone(char *b)
 {
 	int x = 0;
 	unsigned int j = 0;
 	int k = 1;
 	int l = 0;
 
-	while (s[x])
+	while (b[x])
 	{
-		if (s[x] == 45)
+		if (b[x] == 45)
 		{
 			k *= -1;
 		}
 
-		while (s[x] >= 48 && s[x] <= 57)
+		while (b[x] >= 48 && b[x] <= 57)
 		{
 			l = 1;
-			j = (j * 10) + (s[x] - '0');
+			j = (j * 10) + (b[x] - '0');
 			x++;
 		}
 
