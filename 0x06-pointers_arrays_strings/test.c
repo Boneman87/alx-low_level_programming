8116dc05ca68bf23bac5c5700e5f5e045aa8aea3
Saanu <akinniyijoseph@gmail.com>
2022-09-23 21:20:55 -0700

    Another string concatenation

--- 0x06-pointers_arrays_strings/test.c
+++ 0x06-pointers_arrays_strings/test.c
@@ -12,18 +12,17 @@ char *_strncat(char *dest, char *src, int n)
 {
 	int x = 0, y = 0;
 
-	while (*(dest + x) != '\0')\
+	while (*(dest + x) != '\0')
 	{
 		x++;
 	}
 
-	while (y < n)
+	for (y = 0; y < n; y++)
 	{
 		*(dest + x) = *(src + y);
 		if (*(src + y) == '\0')
 			break;
 		x++;
-		y++;
 	}
 	return (dest);
 }
